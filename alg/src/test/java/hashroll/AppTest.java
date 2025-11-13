d9e113d1c91ccdbca3e12a2ed71fa2ee0c4a81a6
Denis <denislee56@gmail.com>
2025-11-13 19:39:31 +0500

    fix(maven): maven imports fix

--- alg/src/test/java/hashroll/AppTest.java
+++ alg/src/test/java/hashroll/AppTest.java
@@ -1,9 +1,6 @@
 package hashroll;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 /**
  * Unit test for simple App.
  */
